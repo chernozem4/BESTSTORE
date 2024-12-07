68282b746617728fddceea1ab92331828a318069
chernozem4 <gaftaf2034@gmail.com>
2024-12-07 11:48:10 +0600

    Add project files.

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,182 @@
+﻿using BESTSTORE.Models;
+using BESTSTORE.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace BESTSTORE.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IWebHostEnvironment environment;
+
+        public ProductsController(ApplicationDbContext context, IWebHostEnvironment environment) 
+        {
+            this.context = context;
+            this.environment = environment;
+        }
+        public IActionResult Index()
+        {
+            var products = context.Products.OrderByDescending(p => p.Id).ToList();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductDto productDto)
+        {
+            if (productDto.ImageFile == null)
+            {
+                ModelState.AddModelError("ImageFile", "The image file is reauired");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(productDto);
+            }
+
+
+            // save the image file
+            string newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            newFileName += Path.GetExtension(productDto.ImageFile!.FileName);
+            string imageFullPath = environment.WebRootPath+"/products/"+ newFileName;
+            using (var stream = System.IO.File.Create(imageFullPath))
+            {
+                productDto.ImageFile.CopyTo(stream);
+            }
+
+            // save the new product in the database
+            Product product = new Product()
+            {
+                Name = productDto.Name,
+                Brand = productDto.Brand,
+                Category = productDto.Category,
+                Price = productDto.Price,
+                Description = productDto.Description,
+                ImageFileName = newFileName,
+                CreatedAt = DateTime.Now,
+            };
+
+            context.Products.Add(product);
+            context.SaveChanges();
+
+            return RedirectToAction("Index","Products");
+        }
+
+
+        public IActionResult Edit(int id) 
+        {
+            var product = context.Products.Find(id);
+
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Products");
+            }
+
+            // create productDto from product
+            var productDto = new ProductDto()
+            {
+                Name = product.Name,
+                Brand = product.Brand,
+                Category = product.Category,
+                Price = product.Price,
+                Description = product.Description,
+            };
+
+
+            ViewData["ProductId"] = product.Id;
+            ViewData["ImageFileName"] = product.ImageFileName;
+            ViewData["CreatedAt"] = product.CreatedAt.ToString("MM/dd/yyyy");
+
+
+            return View(productDto);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(int id, ProductDto productDto)
+        {
+            var product = context.Products.Find(id);
+
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Products");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                ViewData["ProductId"] = product.Id;
+                ViewData["ImageFileName"] = product.ImageFileName;
+                ViewData["CreatedAt"] = product.CreatedAt.ToString("MM/dd/yyyy");
+
+                return View(productDto);
+            }
+
+
+            // Обновляем имя файла изображения, если есть новый файл
+            string newFileName = product.ImageFileName; // Изначально присваиваем имя файла из существующего продукта
+
+            if (productDto.ImageFile != null) // Если новый файл изображения передан
+            {
+                // Генерируем новое уникальное имя файла на основе текущей даты и времени
+                newFileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+                // Добавляем расширение файла из переданного файла
+                newFileName += Path.GetExtension(productDto.ImageFile.FileName);
+
+                // Формируем полный путь к новому файлу на сервере
+                string imageFullPath = environment.WebRootPath + "/products/" + newFileName;
+
+                // Создаем файл на сервере и копируем в него содержимое переданного файла
+                using (var stream = System.IO.File.Create(imageFullPath))
+                {
+                    productDto.ImageFile.CopyTo(stream);
+                }
+
+                // Удаляем старый файл изображения (если он существовал)
+                string oldImageFullPath = environment.WebRootPath + "/products/" + product.ImageFileName;
+                System.IO.File.Delete(oldImageFullPath);
+            }
+
+
+            // update the product in the database
+            product.Name = productDto.Name;
+            product.Brand = productDto.Brand;
+            product.Category = productDto.Category;
+            product.Price = productDto.Price;
+            product.Description = productDto.Description;
+            product.ImageFileName = newFileName;
+
+
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Products");   
+
+        }
+
+
+
+        public IActionResult Delete(int id)
+        {
+            var product = context.Products.Find(id);
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Products");
+            }
+            string imageFullPath = environment.WebRootPath + "/products/" + product.ImageFileName;
+            System.IO.File.Delete(imageFullPath);
+            context.Products.Remove(product);
+            context.SaveChanges(true);
+
+            return RedirectToAction("Index", "Products");
+        }
+
+       
+
+    }
+}
---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BESTSTORE.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BESTSTORE.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
